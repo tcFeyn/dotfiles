5a0e64143507e36d49515336dd09597ff935ae27
David Ferreira <65172017+tcFeyn@users.noreply.github.com>
2022-02-21 19:57:12 +0000

    Config Updated

--- nvim/lua/configs/alpha.lua
+++ nvim/lua/configs/alpha.lua
@@ -31,6 +31,7 @@ dashboard.section.header.val = {
 '                                                       ',
 '                                                       ',
 }
+
 dashboard.section.buttons.val = {
 	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
 	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
